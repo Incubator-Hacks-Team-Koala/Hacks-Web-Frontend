02bf8bf22986a2b7e9096b39dd564ae49221f48f
abluey <zoe1615@hotmail.com>
2023-07-22 23:55:28 +0100

    feat: logout "function"

--- incubator-hacks-app/src/components/profile.vue
+++ incubator-hacks-app/src/components/profile.vue
@@ -1,7 +1,42 @@
 <template>
     <!-- two buttons, top right and top left corners; home and logout -->
+    <!-- basically the header-combo-navbar haha kebab -->
+
+    <header>
+        <router-link to="/"><button class="header-btns home">Home</button></router-link>
+
+        <router-link to="/"><button @:click.once="logout" class="header-btns profile">Logout</button></router-link>
+    </header>
+
     <div>
         AUGH
         ok make pretty later
     </div>
 </template>
+
+<script>
+export default ({
+    methods: {
+        logout() {
+        console.log("LOG OUT")
+        // TODO: replace with actual logout logic
+    }}
+
+})
+</script>
+
+
+<style>
+    .header-btns {
+        font-size: 1rem;
+        position: absolute;
+        top: 1.5rem;
+        padding: 0.5rem;
+    }
+    .home {
+        left: 2rem;
+    }
+    .profile {
+        right: 2rem;
+    }
+</style>
